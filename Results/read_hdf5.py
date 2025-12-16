4a84d4121c87d86f34801f9d421da291afef0024
Tobias Pausch <tobiaspausch@gmx.at>
2025-12-16 22:45:54 +0100

    mmm

--- Results/read_hdf5.py
+++
@@ -1,8 +0,0 @@
-data_file= "/home/tobi/Documents/GW_project/Results/outdir_GW150914_NPE/result/GW150914_data0_1126259462-4_importance_sampling.hdf5"
-import pandas
-import h5py
-
-data = h5py.File(data_file, 'r')
-print(data["samples"])#
-df = pandas.DataFrame(data["samples"][:])
-print(df.head())
