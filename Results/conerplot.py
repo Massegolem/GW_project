d0ed482d87378f929b8ea3fb10de3a49e8555088
Tobias Pausch <tobiaspausch@gmx.at>
2025-12-19 13:25:43 +0100

    S model results

--- Results/conerplot.py
+++ Results/conerplot.py
@@ -11,8 +11,8 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
 
-data_file1= "/home/tobi/Documents/GW_project/Results/outdir_GW150914_NPE/result/GW150914_data0_1126259462-4_importance_sampling.hdf5"
-data_file2= "/home/tobi/Documents/GW_project/Results/outdir_GW150914_NPE/result/GW150914_data0_1126259462-4_sampling.hdf5"
+data_file1= "/home/tobi/Documents/GW_project/Results/outdir_GW150914_NPE_S/result/GW150914_data0_1126259462-4_importance_sampling.hdf5"
+data_file2= "/home/tobi/Documents/GW_project/Results/outdir_GW150914_NPE_S/result/GW150914_data0_1126259462-4_sampling.hdf5"
 data_file3= "/home/tobi/Documents/GW_project/Results/outdir_GW150914_toy/result/GW150914_data0_1126259462-4_sampling.hdf5"
 
 gwtc1_medians = {
@@ -51,16 +51,16 @@ label_map = {
 
 bins=15
 labels = [label_map[p] for p in params]
-fig = corner.corner(df1[params], bins=bins,  labels=labels, weights=weights1, truths=truths, truth_color="black", color="C0")
-corner.corner(df2[params], bins=bins, fig=fig, labels=labels, color="C1")
-corner.corner(df3[params], bins=bins, fig=fig, labels=labels, color="C2")
-corner.corner(df1[params], bins=bins, fig=fig, weights=weights1, color="C0")
+
+fig = corner.corner(df3[params], bins=bins,  labels=labels, truths=truths, truth_color="black", color="C0")
+corner.corner(df2[params], bins=bins, fig=fig, labels=labels, color="C2")
+corner.corner(df1[params], bins=bins, fig=fig, labels=labels, weights=weights1, color="C1")
 
 
 legend_elements = [
-    Line2D([0], [0], color="C0", lw=2, label="NPE small Importance sampling"),
-    Line2D([0], [0], color="C1", lw=2, label="NPE small"),
-    Line2D([0], [0], color="C2", lw=2, label="NPE Toy model"),
+    Line2D([0], [0], color="C0", lw=2, label="NPE Toy model"),
+    Line2D([0], [0], color="C1", lw=2, label="NPE small Importance sampling"),
+    Line2D([0], [0], color="C2", lw=2, label="NPE small"),
 ]
 
 
