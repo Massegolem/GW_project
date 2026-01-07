41f5b7df6aaad82da436d0efe763e8d6c7fecb0e
Massegolem <tobiaspausch@gmx.at>
2026-01-07 12:00:54 +0100

    mm

---
+++ Results/outdir_GW150914_NPE_S3/submit/bash_GW150914.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# GW150914_data0_1126259462-4_generation
+# PARENTS 
+# CHILDREN GW150914_data0_1126259462-4_sampling GW150914_data0_1126259462-4_importance_sampling
+if [[ "GW150914_data0_1126259462-4_generation" == *"$1"* ]]; then
+    echo "Running: /home/tobi/Documents/GW_project/venvs/NPE_dingo_venv2/bin/dingo_pipe_generation outdir_GW150914/GW150914_config_complete.ini --label GW150914_data0_1126259462-4_generation --idx 0 --trigger-time 1126259462.4 --outdir outdir_GW150914"
+    /home/tobi/Documents/GW_project/venvs/NPE_dingo_venv2/bin/dingo_pipe_generation outdir_GW150914/GW150914_config_complete.ini --label GW150914_data0_1126259462-4_generation --idx 0 --trigger-time 1126259462.4 --outdir outdir_GW150914
+fi
+
+# GW150914_data0_1126259462-4_sampling
+# PARENTS GW150914_data0_1126259462-4_generation
+# CHILDREN GW150914_data0_1126259462-4_importance_sampling
+if [[ "GW150914_data0_1126259462-4_sampling" == *"$1"* ]]; then
+    echo "Running: /home/tobi/Documents/GW_project/venvs/NPE_dingo_venv2/bin/dingo_pipe_sampling outdir_GW150914/GW150914_config_complete.ini --label GW150914_data0_1126259462-4_sampling --event-data-file outdir_GW150914/data/GW150914_data0_1126259462-4_generation_event_data.hdf5 --outdir outdir_GW150914"
+    /home/tobi/Documents/GW_project/venvs/NPE_dingo_venv2/bin/dingo_pipe_sampling outdir_GW150914/GW150914_config_complete.ini --label GW150914_data0_1126259462-4_sampling --event-data-file outdir_GW150914/data/GW150914_data0_1126259462-4_generation_event_data.hdf5 --outdir outdir_GW150914
+fi
+
+# GW150914_data0_1126259462-4_importance_sampling
+# PARENTS GW150914_data0_1126259462-4_sampling GW150914_data0_1126259462-4_generation
+# CHILDREN GW150914_data0_1126259462-4_importance_sampling_plot
+if [[ "GW150914_data0_1126259462-4_importance_sampling" == *"$1"* ]]; then
+    echo "Running: /home/tobi/Documents/GW_project/venvs/NPE_dingo_venv2/bin/dingo_pipe_importance_sampling outdir_GW150914/GW150914_config_complete.ini --outdir outdir_GW150914 --label GW150914_data0_1126259462-4_importance_sampling --proposal-samples-file outdir_GW150914/result/GW150914_data0_1126259462-4_sampling.hdf5 --event-data-file outdir_GW150914/data/GW150914_data0_1126259462-4_generation_event_data.hdf5"
+    /home/tobi/Documents/GW_project/venvs/NPE_dingo_venv2/bin/dingo_pipe_importance_sampling outdir_GW150914/GW150914_config_complete.ini --outdir outdir_GW150914 --label GW150914_data0_1126259462-4_importance_sampling --proposal-samples-file outdir_GW150914/result/GW150914_data0_1126259462-4_sampling.hdf5 --event-data-file outdir_GW150914/data/GW150914_data0_1126259462-4_generation_event_data.hdf5
+fi
+
+# GW150914_data0_1126259462-4_importance_sampling_plot
+# PARENTS GW150914_data0_1126259462-4_importance_sampling
+# CHILDREN 
+if [[ "GW150914_data0_1126259462-4_importance_sampling_plot" == *"$1"* ]]; then
+    echo "Running: /home/tobi/Documents/GW_project/venvs/NPE_dingo_venv2/bin/dingo_pipe_plot --label GW150914_data0_1126259462-4_importance_sampling_plot --result outdir_GW150914/result/GW150914_data0_1126259462-4_importance_sampling.hdf5 --outdir outdir_GW150914/result --corner --weights --log_probs"
+    /home/tobi/Documents/GW_project/venvs/NPE_dingo_venv2/bin/dingo_pipe_plot --label GW150914_data0_1126259462-4_importance_sampling_plot --result outdir_GW150914/result/GW150914_data0_1126259462-4_importance_sampling.hdf5 --outdir outdir_GW150914/result --corner --weights --log_probs
+fi
+
