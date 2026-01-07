debfdd5146f81f891b942d5c5a2676c831a77ae7
Tobias Pausch <tobiaspausch@gmx.at>
2026-01-07 13:40:25 +0100

    sort stuff and created pictures

--- Results/conerplot.py
+++ Results/conerplot.py
@@ -11,9 +11,9 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
 
-data_file1= "/home/tobi/Documents/GW_project/Results/outdir_GW150914_NPE_S/result/GW150914_data0_1126259462-4_importance_sampling.hdf5"
-data_file2= "/home/tobi/Documents/GW_project/Results/outdir_GW150914_NPE_S/result/GW150914_data0_1126259462-4_sampling.hdf5"
-data_file3= "/home/tobi/Documents/GW_project/Results/outdir_GW150914_toy/result/GW150914_data0_1126259462-4_sampling.hdf5"
+data_file1= "/home/tobi/Documents/GW_project/Results/outdir/outdir_GW150914_NPE_S/result/GW150914_data0_1126259462-4_sampling.hdf5"
+data_file2= "/home/tobi/Documents/GW_project/Results/outdir/outdir_GW150914_NPE_S2/result/GW150914_data0_1126259462-4_sampling.hdf5"
+data_file3= "/home/tobi/Documents/GW_project/Results/outdir/outdir_GW150914_NPE_S3/result/GW150914_data0_1126259462-4_sampling.hdf5"
 
 gwtc1_medians = {
     "chirp_mass": 28.6,
@@ -28,7 +28,7 @@ gwtc1_medians = {
 with h5py.File(data_file1, "r") as f:
     df1 = pd.DataFrame(f["samples"][:])
 
-weights1 = df1["weights"] 
+#weights1 = df1["weights"] 
 
 
 with h5py.File(data_file2, "r") as f:
@@ -52,15 +52,15 @@ label_map = {
 bins=15
 labels = [label_map[p] for p in params]
 
-fig = corner.corner(df3[params], bins=bins,  labels=labels, truths=truths, truth_color="black", color="C0")
-corner.corner(df2[params], bins=bins, fig=fig, labels=labels, color="C2")
-corner.corner(df1[params], bins=bins, fig=fig, labels=labels, weights=weights1, color="C1")
-
+fig = corner.corner(df1[params], bins=bins,  labels=labels, truths=truths, truth_color="black", color="C0")
+corner.corner(df3[params], bins=bins, fig=fig, labels=labels, color="C2")
+#corner.corner(df1[params], bins=bins, fig=fig, labels=labels, weights=weights1, color="C1")
+corner.corner(df2[params], bins=bins, fig=fig, labels=labels, color="C1")
 
 legend_elements = [
-    Line2D([0], [0], color="C0", lw=2, label="NPE Toy model"),
-    Line2D([0], [0], color="C1", lw=2, label="NPE small Importance sampling"),
-    Line2D([0], [0], color="C2", lw=2, label="NPE small"),
+    Line2D([0], [0], color="C0", lw=2, label="NPE_S"),
+    Line2D([0], [0], color="C1", lw=2, label="NPE_S2"),
+    Line2D([0], [0], color="C2", lw=2, label="NPE_S3"),
 ]
 
 
